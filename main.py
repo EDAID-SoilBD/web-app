f1a3cf559867f559df134d831e819373d89b11f2
Oleg Brezitskyy <brezitskyyoleg@gmail.com>
2021-10-14 00:24:55 +0200

    Added search data service

--- main.py
+++ main.py
@@ -17,5 +17,11 @@ def browse_data():
     return render_template("browse.html", records=r, colnames=r[0].keys())
 
 
+@app.route("/data/<code>")
+def search_data(code):
+    r = get(f"http://localhost:5000/data/{code}").json()
+    return render_template("browse.html", records=[r], colnames=r.keys())
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4000)
